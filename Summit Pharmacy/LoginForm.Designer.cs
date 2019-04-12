ca0823780f6537ced0cbecdecb09d1a68edd246b
filcan <soyakzx@gmail.com>
2019-04-12 13:31:57 +0800

    Login Form UI

--- Summit Pharmacy/LoginForm.Designer.cs
+++ Summit Pharmacy/LoginForm.Designer.cs
@@ -28,20 +28,172 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.textBox_Username = new System.Windows.Forms.TextBox();
+            this.textBox_Password = new System.Windows.Forms.TextBox();
+            this.button_Login = new System.Windows.Forms.Button();
+            this.label_Username = new System.Windows.Forms.Label();
+            this.label_Password = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.toolTip = new System.Windows.Forms.ToolTip(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button_Minimize = new System.Windows.Forms.Button();
+            this.button_Close = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // textBox_Username
+            // 
+            this.textBox_Username.Font = new System.Drawing.Font("Segoe UI Light", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_Username.ForeColor = System.Drawing.Color.Black;
+            this.textBox_Username.Location = new System.Drawing.Point(104, 167);
+            this.textBox_Username.Name = "textBox_Username";
+            this.textBox_Username.Size = new System.Drawing.Size(155, 25);
+            this.textBox_Username.TabIndex = 0;
+            // 
+            // textBox_Password
+            // 
+            this.textBox_Password.Font = new System.Drawing.Font("Segoe UI Light", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_Password.ForeColor = System.Drawing.Color.Black;
+            this.textBox_Password.Location = new System.Drawing.Point(104, 200);
+            this.textBox_Password.Name = "textBox_Password";
+            this.textBox_Password.Size = new System.Drawing.Size(155, 25);
+            this.textBox_Password.TabIndex = 1;
+            // 
+            // button_Login
+            // 
+            this.button_Login.FlatAppearance.BorderSize = 0;
+            this.button_Login.Font = new System.Drawing.Font("Segoe UI Light", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_Login.ForeColor = System.Drawing.Color.Black;
+            this.button_Login.Location = new System.Drawing.Point(104, 231);
+            this.button_Login.Name = "button_Login";
+            this.button_Login.Size = new System.Drawing.Size(155, 31);
+            this.button_Login.TabIndex = 2;
+            this.button_Login.Text = "Login";
+            this.button_Login.UseVisualStyleBackColor = true;
+            // 
+            // label_Username
+            // 
+            this.label_Username.AutoSize = true;
+            this.label_Username.Font = new System.Drawing.Font("Segoe UI Light", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_Username.ForeColor = System.Drawing.Color.Black;
+            this.label_Username.Location = new System.Drawing.Point(23, 170);
+            this.label_Username.Name = "label_Username";
+            this.label_Username.Size = new System.Drawing.Size(63, 17);
+            this.label_Username.TabIndex = 3;
+            this.label_Username.Text = "Username";
+            // 
+            // label_Password
+            // 
+            this.label_Password.AutoSize = true;
+            this.label_Password.Font = new System.Drawing.Font("Segoe UI Light", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_Password.ForeColor = System.Drawing.Color.Black;
+            this.label_Password.Location = new System.Drawing.Point(23, 203);
+            this.label_Password.Name = "label_Password";
+            this.label_Password.Size = new System.Drawing.Size(57, 17);
+            this.label_Password.TabIndex = 4;
+            this.label_Password.Text = "Password";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(134)))), ((int)(((byte)(82)))));
+            this.panel1.Controls.Add(this.button_Minimize);
+            this.panel1.Controls.Add(this.button_Close);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(284, 27);
+            this.panel1.TabIndex = 5;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Black;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 27);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(284, 1);
+            this.panel2.TabIndex = 6;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Summit_Pharmacy.Properties.Resources.img_summit_pharmacy_green;
+            this.pictureBox1.Location = new System.Drawing.Point(17, 39);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(250, 120);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button_Minimize
+            // 
+            this.button_Minimize.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button_Minimize.FlatAppearance.BorderSize = 0;
+            this.button_Minimize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_Minimize.Image = global::Summit_Pharmacy.Properties.Resources.ic_minimize_white_18dp;
+            this.button_Minimize.Location = new System.Drawing.Point(194, 0);
+            this.button_Minimize.Name = "button_Minimize";
+            this.button_Minimize.Size = new System.Drawing.Size(45, 27);
+            this.button_Minimize.TabIndex = 1;
+            this.button_Minimize.TabStop = false;
+            this.toolTip.SetToolTip(this.button_Minimize, "Minimize");
+            this.button_Minimize.UseVisualStyleBackColor = true;
+            // 
+            // button_Close
+            // 
+            this.button_Close.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button_Close.FlatAppearance.BorderSize = 0;
+            this.button_Close.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(15)))), ((int)(((byte)(29)))));
+            this.button_Close.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(17)))), ((int)(((byte)(35)))));
+            this.button_Close.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_Close.Image = global::Summit_Pharmacy.Properties.Resources.ic_close_white_18dp;
+            this.button_Close.Location = new System.Drawing.Point(239, 0);
+            this.button_Close.Name = "button_Close";
+            this.button_Close.Size = new System.Drawing.Size(45, 27);
+            this.button_Close.TabIndex = 0;
+            this.button_Close.TabStop = false;
+            this.toolTip.SetToolTip(this.button_Close, "Close");
+            this.button_Close.UseVisualStyleBackColor = true;
             // 
             // LoginForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(219)))), ((int)(((byte)(214)))), ((int)(((byte)(209)))));
+            this.ClientSize = new System.Drawing.Size(284, 301);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label_Password);
+            this.Controls.Add(this.label_Username);
+            this.Controls.Add(this.button_Login);
+            this.Controls.Add(this.textBox_Password);
+            this.Controls.Add(this.textBox_Username);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "LoginForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Summit Pharmacy";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox_Username;
+        private System.Windows.Forms.TextBox textBox_Password;
+        private System.Windows.Forms.Button button_Login;
+        private System.Windows.Forms.Label label_Username;
+        private System.Windows.Forms.Label label_Password;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button_Close;
+        private System.Windows.Forms.Button button_Minimize;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ToolTip toolTip;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
